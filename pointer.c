63c5b9452fb01e81b5c703972448b57303021700
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-06-28 21:00:30 +0600

    Different way to pointer variable declaration and print memory address

--- pointer.c
+++ pointer.c
@@ -3,8 +3,13 @@
 int main() {
 
     int exampleValue = 10;
+    int *pointerVariable = &exampleValue;
+    int *anotherPointerVariable;
+    anotherPointerVariable = &exampleValue;
     printf("%d\n", exampleValue);
     printf("%p\n", &exampleValue);
-    
+    printf("%p\n", pointerVariable);
+    printf("%p\n", anotherPointerVariable);
+
     return 0;
 }
