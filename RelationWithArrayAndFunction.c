0fb60bc8e8fd6915adb9ba7b9bc410f4c285cb25
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-07-01 11:30:22 +0600

    Pointer- Dynamic memory allocation, malloc(), calloc(), free(), realloc()

--- RelationWithArrayAndFunction.c
+++ RelationWithArrayAndFunction.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int *num1, int *num2) {
+    int temp = *num1;
+    num1 = *num2;
+    num2 = temp;
+
+    printf("After swapping num1 = %d\n num2 = %d\n", *num1, *num2);
+}
+
 int main() {
 
     int a[5] = {1, 2, 3, 4, 5};
@@ -22,5 +30,10 @@ int main() {
     printf("%d\n", *ptr + 1);
     printf("Address of index 5 : %u\n", (ptr + 1));
     printf("%d", *ptr - 1);
+
+    int num1 = 7, num2 = 8;
+    printf("Before swapping : %d, %d", num1, num2);
+    swap(&num1, &num2);
+
     return 0;
 }
