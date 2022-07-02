60528531646753eef524438582ed4761bbae9623
Md. Shafiul Islam Sohan <sohan110111@gmail.com>
2022-07-03 02:43:39 +0600

    linked list traversing

---
+++ LinkedList.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct LinkedList
+{
+    int data;
+    struct LinkedList *next;
+};
+
+
+int main() {
+
+    struct LinkedList *list = NULL;          
+    struct LinkedList *list_2 = NULL;          
+    struct LinkedList *list_3 = NULL;
+
+    list = (struct LinkedList*)malloc(sizeof(struct LinkedList));   
+    list_2 = (struct LinkedList*)malloc(sizeof(struct LinkedList));   
+    list_3 = (struct LinkedList*)malloc(sizeof(struct LinkedList));
+
+    list -> data = 4;
+    list_2 -> data = 5;
+    list_3 -> data = 6;
+
+    list -> next = list_2;
+    list_2 -> next = list_3;
+    list_3 -> next = NULL;
+
+    while (list != NULL) {
+        
+        printf("%d -> ", list -> data);
+        list = list->next;
+    }
+
+    return 0;
+}
