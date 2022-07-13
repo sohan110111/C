b90bb250805d60c6a5da638515c33d689e5805a6
Binary01 <binary01.dev@gmail.com>
2022-07-13 23:42:24 +0600

    LinkedList create from array and traverse

--- LinkedListFromArray.c
+++ LinkedListFromArray.c
@@ -25,8 +25,26 @@ struct node *linkedlist_insert(int a[], int size) {
     }
 
     return head;
-    
+}
 
+struct node *reversedLinkedListNode(struct node *head) {
+    struct node *prev = NULL, *current = NULL, *next = NULL;
+
+    while (current != NULL)
+    {
+        //store the next node
+        next = current->next;
+        //reverse the link/connection
+        current->next = prev;
+        //propagate the link/connection
+        prev = current;
+        current = next;
+    }
+    head = prev;
+    while (head != NULL) {
+        printf("%d -> ", head-> data);
+        head = head->next; 
+    }
 }
 
 int indexSearch(struct node *head, int value) {
@@ -48,6 +66,7 @@ int main() {
     int array[] = {0,1,2,3,4,5,6};
     struct node *head = linkedlist_insert(array, sizeof(array)/sizeof(array[0]));
     struct node *newHead = head;
+    struct node *newHead2 = head;
     while (head != NULL) {
         printf("%d -> ", head->data);
         head = head->next;
@@ -56,8 +75,10 @@ int main() {
     printf("NULL");
     int searchValue = 5;
 
-    printf("\nvalue of position is %d", indexSearch(head, 5));
+    printf("\nsearch value %d of position is %d", searchValue, indexSearch(newHead, searchValue));
     
+    //reversed linked list 
+    reversedLinkedListNode(newHead2);
 
     return 0;
 }
