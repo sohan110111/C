32e0d84efb48273c2ebf44349b6e9f80b12221a1
sohan110111 <sohan110111@gmail.com>
2023-02-02 08:32:50 +0600

    memory address and value

--- memoryAddress.c
+++ memoryAddress.c
@@ -3,12 +3,16 @@ int main()
 {
     int a = 10;
     int *p = &a;
-    
+
     printf("Address of a: %p\n", &a);
     printf("Address of p: %p\n", &p);
     printf("Value of p: %p\n", p);
     printf("Value of a: %d\n", a);
     printf("Value of a: %d\n", *p);
+
+    int *pp = p;
+    printf("Address of *p: %p\n", pp);
+    printf("Value of p: %d\n", *pp);
 
     return 0;
 }
